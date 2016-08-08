9287608c76c5c8e84d4bf46e25a29a17460880d2
iurisman <igor.urisman@gmail.com>
2016-08-08 12:22:41 -0700

    JAVADOC c.v.client.servlet.VariantFilter

--- CLIENT-JAVA/src/main/java/com/variant/client/servlet/VariantFilter.java
+++ CLIENT-JAVA/src/main/java/com/variant/client/servlet/VariantFilter.java
@@ -26,15 +26,94 @@ import com.variant.core.schema.parser.ParserResponse;
 import com.variant.core.schema.parser.ParserMessage.Severity;
 
 /**
- * Variant servlet filter is the preferred way of utilizing Variant servlet adapter.
- * By using this filter, the application programmer can, in many cases, instrument experiments
- * with no to very little coding. The filter intercepts all HTTP requests for the instrumented
- * pages, obtains the ...
+ * <p>The preferred way of instrumenting Variant experiments for host applications written on top of the Java Servlet API.
  * 
- * The filter is configured as fo
+ * <h4>Overview</h4> 
+ *
+ * <p>By using this filter, the application programmer can, in many cases, instrument experiments
+ * with no to very little coding. The filter intercepts HTTP requests for the instrumented
+ * pages, obtains the resolved variant and, if non-control, forwards the request to the path
+ * contained in the resolved state parameter {@code path}.
+ * 
+ * <p>The application programmer should use this filter if the following assumptions hold:
+ * <ul>
+ * <li>The host applicaiton is written on top of the Servlet API.
+ * <li>All state variants have a single point of entry, which can be mapped to a request path.
+ * <li>The test schema defines the {@code path} state parameter, so that 1) its base value 
+ * denotes the resource path to the this state's control; and 2) its variant value denotes the 
+ * resource path to the single point of entry of that state variant.
+ * </ul>
+ * 
+ * <h4>Configuration</h4>
+ * <p>The filter is configured as following:
+ * <pre>
+ * {@code
+ *  <filter>
+ *      <filter-name>variantFilter</filter-name>
+ *      <filter-class>com.variant.client.servlet.VariantFilter</filter-class> 
+ *      <init-param>
+ *          <param-name>schemaResourceName</param-name>
+ *          <param-value>/path/to/schema.json</param-value>
+ *      </init-param>
+ *      <init-param>
+ *          <param-name>propsResourceName</param-name>
+ *          <param-value>/path/to/variant.props</param-value>
+ *      </init-param>
+ *  </filter>
+ *   
+ *  <filter-mapping>
+ *      <filter-name>variantFilter</filter-name>
+ *      <url-pattern>/*</url-pattern>
+ *  </filter-mapping>
+ * }
+ * </pre>
+ *
+ * The {@link #init(FilterConfig)} method looks for two initialization parameters:
+ * <ul>
+ * <li>{@code schemaResourceName}: Name of the schema file as a classpath resource. 
+ * Parsed once during filter initialization. To change a schema, application restart will be required. 
+ * (Temporary limitaion to be removed in an upcoming version.)
+ * 
+ * <li>{@code propsResourceName}: Name of the application properties file as a
+ * classpath resource. Its content will override default properties, but will be overridden
+ * by {@code /varaint.props}, if found on the classpath.
+ * </ul>
+ * 
+ * <p>The URL pattern in filter mapping can be something narrower than {@code /*}, of course, so long
+ * as it matches all state {@code path}s listed in the schema.
+ * 
+ * <h4>Execution Semantics</h4>
+ * 
+ * <p>It is assumed that the base {@code path} state parameter, i.e. the one specified at the {@code State}
+ * level, denotes the resource path to the control variation of this state. This allows this filter to
+ * identify whether an incoming HTTP request is for an instrumented state or not.  If not, the request
+ * is forwarded down the filter chain by calling {@code chain.doFilter(ServletRequest, ServletResponse)}.
+ * 
+ * <p>If the requested path corresponds to an instrumented state, the session (obtained from 
+ * Variant client servlet adapter by calling {@link VariantServletClient#getOrCreateSession(HttpServletRequest)}) 
+ * is targeted for this state with {@link VariantServletSession#targetForState(State)}. The resulting
+ * {@link VariantServletStateRequest} object contains information about the outcome of the targeting
+ * operation, including the resulting variant and the resolved state parameters. This 
+ * {@link VariantServletStateRequest} object is added to the current {@link HttpServletRequest} as
+ * an attribute named {@link #VARIANT_REQUEST_ATTRIBUTE_NAME}, should the downstream application code 
+ * wish to extend the semantics, e.g. trigger a custom {@link VariantEvent}.  
+ * 
+ * <p>The resolved variant's {@code path} 
+ * state parameter is interpreted as the request path to the resource which implements the targeted
+ * variant. The request is forwarded to that path with {@code ServletRequestDispatcher.forward()}.
+ * 
+ * <p>Upon return from either forward or a fall-through down the filter chain, the 
+ * {@link #doFilter(ServletRequest, ServletResponse, FilterChain)} method below adds the status of the
+ * HTTP response in progress to the pending state visited event and commits the Variant state request
+ * in progress.
+ * 
+ * <p>Any exceptions due to Variant are caught and logged. Should such an exception occur, an attempt
+ * is made to mark the state of the pending state visited event as failed and to allow the request
+ * proceed down the filter chain. In other words, should the instrumentation fail due to an internal
+ * Varaint exception, the user session will see the control experience. 
  * 
  * @author Igor Urisman
- *
+ * @since 0.5
  */
 public class VariantFilter implements Filter {
 
@@ -46,9 +125,11 @@ public class VariantFilter implements Filter {
 	//                                          PUBLIC                                             //
 	//---------------------------------------------------------------------------------------------//
 
+	public static final String VARIANT_REQUEST_ATTRIBUTE_NAME = "variant-state-request";
 	/**
-	 * Webapp is starting up.
-	 * Initialize the Variant container and parse the test schema.
+	 * Initialize the Variant servlet adapter and parse the experiment schema.
+	 * @see Filter#init(FilterConfig)
+	 * @since 0.5
 	 */
 	@Override
 	public void init(FilterConfig config) throws ServletException {
@@ -84,10 +165,9 @@ public class VariantFilter implements Filter {
 	}
 
 	/**
-	 * Request.
-	 * It is critical that we catch all Variant related exceptions and
-	 * simply fall back to the control experience, i.e. the one that was
-	 * requested.
+	 * Identify the state, target and commit the state request.
+	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
+	 * @since 0.5
 	 */
 	@Override
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) 
@@ -111,30 +191,33 @@ public class VariantFilter implements Filter {
 			
 			if (state == null) {
 
-				// We don't know about this path.
-				if (LOG.isDebugEnabled()) {
-					LOG.debug("Path [" + url + "] is not instrumented");
+				// Variant doesn't know about this path.
+				if (LOG.isTraceEnabled()) {
+					LOG.trace("Path [" + url + "] is not instrumented");
 				}
 			
 			}
 			else {
 			
-				// Yes, this path is mapped in Variant.
+				// This path is instrumented by Variant.
 				variantSsn = client.getOrCreateSession(httpRequest);
 				variantRequest = variantSsn.targetForState(state);
+				httpRequest.setAttribute(VARIANT_REQUEST_ATTRIBUTE_NAME, variantRequest);
 				resolvedPath = variantRequest.getResolvedParameterMap().get("path");
 				isForwarding = !resolvedPath.equals(state.getParameterMap().get("path"));
 				
-				String msg = 
-						"Variant dispatcher for URL [" + url +
-						"] completed in " + DurationFormatUtils.formatDuration(System.currentTimeMillis() - start, "mm:ss.SSS") +". ";
-				if (isForwarding) {
-					msg += "Forwarding to path [" + resolvedPath + "].";							
+				if (LOG.isDebugEnabled()) {
+					String msg = 
+							"Variant dispatcher for URL [" + url +
+							"] completed in " + DurationFormatUtils.formatDuration(System.currentTimeMillis() - start, "mm:ss.SSS") +". ";
+					if (isForwarding) {
+						msg += "Forwarding to path [" + resolvedPath + "].";							
+					}
+					else {
+						msg += "Falling through to requested URL";
+					}
+					LOG.debug(msg);
 				}
-				else {
-					msg += "Falling through to requested URL";
-				}
-				LOG.info(msg);
 				
 			}
 		}
@@ -171,8 +254,8 @@ public class VariantFilter implements Filter {
 	}
 
 	/**
-	 * Webapp is shutting down.
-	 * Shutdown the Variant engine to ensure that all in-flight events are flushed.
+	 * @see Filter#destroy()
+	 * @since 0.5
 	 */
 	@Override
 	public void destroy() {
