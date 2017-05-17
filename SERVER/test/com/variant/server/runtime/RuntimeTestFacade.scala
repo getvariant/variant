a8e6eba1fd6be95c9e15cc7bc9c5fb2dc6807833
iurisman <igor.urisman@gmail.com>
2017-05-16 18:19:47 -0700

    #64

--- SERVER/test/com/variant/server/runtime/RuntimeTestFacade.scala
+++ SERVER/test/com/variant/server/runtime/RuntimeTestFacade.scala
@@ -7,6 +7,9 @@ import com.variant.server.boot.VariantServer
 import java.util.Collection
 import com.variant.core.schema.impl.StateVariantImpl
 import scala.collection.mutable.ArrayBuffer
+import scala.collection.mutable.ListBuffer
+import java.util.ArrayList
+import com.variant.core.schema.Test
 
 /**
  * Test facade extends scope of non-public methods to public for testability.
@@ -16,8 +19,36 @@ object RuntimeTestFacade {
 }
 
 class RuntimeTestFacade(server: VariantServer)  extends Runtime(server) {
+
+   /**
+    * 
+    */
    def resolveState(state: State, vector: Array[Experience]): (Boolean, StateVariantImpl) = {
       val result = super.resolveState(state, vector.toSeq)
-      return (result._1(), result._2())
+      (result._1(), result._2())
+   }
+   
+   /**
+    * 
+    */
+   def isResolvable(vector: Array[Experience]): Boolean = {
+      super.isResolvable(vector.toSeq)
+   }
+   
+   /**
+    * Second argument may change as a side effect, so must be mutable.
+    * Take Scala types as params because of the consice way the caller can instantiate them,
+    * but here we convert to a Java collection because Scala iterators don't support the remove()
+    * mothod which is used in the underlying implementation.
+    */
+   def minUnresolvableSubvector(v: Array[Experience], w: Array[Experience]) = {
+      super.minUnresolvableSubvector(new ArrayList(v.toSeq),new ArrayList(w.toSeq)).toArray()
+   }
+   
+   /*
+    * 
+    */
+   def isTargetable(test: Test, state: State,  alreadyTargetedExperiences: Array[Experience]): Boolean = {
+      super.isTargetable(test, state, alreadyTargetedExperiences.toSeq)
    }
 }
