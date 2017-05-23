1e53df5e3b6c1b2650e26e53b3519a7490110ee0
iurisman <igor.urisman@gmail.com>
2017-05-23 12:05:58 -0700

    Add splash message at /

--- SERVER/test/com/variant/server/test/ServerTest.scala
+++ SERVER/test/com/variant/server/test/ServerTest.scala
@@ -3,6 +3,7 @@ package com.variant.server.test
 import org.scalatestplus.play._
 import play.api.test._
 import play.api.test.Helpers._
+import com.variant.server.boot.VariantServer
 
 /**
  * Add your spec here.
@@ -11,12 +12,22 @@ import play.api.test.Helpers._
  */
 class ServerTest extends BaseSpecWithServer {
 
-  "Routes" should {
+   "Routes" should {
 
-    "send 404 on a bad request" in  {
-       val resp = route(app, FakeRequest(GET, "/session/foo")).get
-       status(resp) mustBe NOT_FOUND
-       contentAsString(resp) mustBe empty
-    }
-  }
+      "send 404 on a bad request" in  {
+         val resp = route(app, FakeRequest(GET, context + "/bad")).get
+         status(resp) mustBe NOT_FOUND
+         contentAsString(resp) mustBe empty
+      }
+    
+      "send splash on a root request" in  {
+         println(context)
+         val resp = route(app, FakeRequest(GET, context)).get
+         status(resp) mustBe OK
+         println(contentAsString(resp))
+         contentAsString(resp) must startWith (VariantServer.server.productName)
+         contentAsString(resp) must include ("Uptime")
+      }
+      
+   }
 }
