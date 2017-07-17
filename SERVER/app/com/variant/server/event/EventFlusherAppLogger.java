00b73bf7fb624dd2899e5b928bd298004f53c083
iurisman <igor.urisman@gmail.com>
2017-07-17 14:55:18 -0700

    Convert to single JSON.

--- SERVER/app/com/variant/server/event/EventFlusherAppLogger.java
+++ SERVER/app/com/variant/server/event/EventFlusherAppLogger.java
@@ -34,43 +34,46 @@ public class EventFlusherAppLogger implements EventFlusher {
 
 		for (FlushableEvent event: events) {
 			StringBuilder msg = new StringBuilder();
-			msg.append("EVENT:{")
-			.append("session_id:'").append(event.getSession().getId()).append("', ")
-			.append("created_on:'").append(event.getCreateDate()).append("', ")
-			.append("event_name:'").append(event.getName()).append("', ")
-			.append("event_value:'").append(event.getValue()).append("'")
-			.append("}");
+			msg.append("{")
+         .append("event_name:'").append(event.getName()).append("', ")
+			.append("created_on:'").append(event.getCreateDate().getTime()).append("', ")
+			.append("event_value:'").append(event.getValue()).append("', ")
+         .append("session_id:'").append(event.getSession().getId()).append("'");
+
+			if (!event.getLiveExperiences().isEmpty()) {
+			   msg.append(", event_experiences:[");
+			   boolean first = true;
+   			for (Experience e: event.getLiveExperiences()) {
+   			   if (first) first = false;
+   			   else msg.append(", ");
+   			   msg.append("{")
+               .append("test_name:'").append(e.getTest().getName()).append("', ")
+               .append("experience_name:'").append(e.getName()).append("', ")
+               .append("is_control:").append(e.isControl())
+               .append("}");
+            }
+   			msg.append("]");
+			}			
+			
+	      if (!event.getParameterMap().isEmpty()) {
+            msg.append(", event_params:[");
+            boolean first = true;
+	         for (Map.Entry<String, String> param: event.getParameterMap().entrySet()) {
+	            if (first) first = false;
+	            else msg.append(", ");
+	            msg.append("{")
+	            .append("key:'").append(param.getKey()).append("', ")
+	            .append("value:'").append(param.getValue()).append("'")
+	            .append("}");
+	         }
+	         msg.append("]");
+	      }     
+
+	      msg.append("}");
 
 			LOG.info(msg.toString());
 		}
 								
-		for (FlushableEvent event: events) {
-			for (Experience e: event.getLiveExperiences()) {
-				StringBuilder msg = new StringBuilder();
-				msg.append("EVENT_EXPERIENCES:{")
-				.append("event_name:'").append(event.getName()).append("', ")
-				.append("test_name:'").append(e.getTest().getName()).append("', ")
-				.append("experience_name:'").append(e.getName()).append("', ")
-				.append("is_control:").append(e.isControl()).append("', ")
-				.append("}");
-
-				LOG.info(msg.toString());
-			}
-		}
-		
-		for (FlushableEvent event: events) {
-			for (Map.Entry<String, String> param: event.getParameterMap().entrySet()) {
-
-				StringBuilder msg = new StringBuilder();
-				msg.append("EVENT_PARAMS:{")
-				.append("event_name:'").append(event.getName()).append("', ")
-				.append("key:'").append(param.getKey()).append("', ")
-				.append("value:'").append(param.getValue()).append("'")
-				.append("}");
-
-				LOG.info(msg.toString());
-			}
-		}		
 	}
 
 }
