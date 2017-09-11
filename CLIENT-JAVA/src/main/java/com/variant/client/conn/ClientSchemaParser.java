a428089e9212fd8fe57d1a052ff2137fe2610ba8
iurisman <igor.urisman@gmail.com>
2017-09-11 15:28:50 -0700

    Add flusher service to client side schema parser.

--- CLIENT-JAVA/src/main/java/com/variant/client/conn/ClientSchemaParser.java
+++ CLIENT-JAVA/src/main/java/com/variant/client/conn/ClientSchemaParser.java
@@ -1,11 +1,13 @@
 package com.variant.client.conn;
 
+import com.variant.core.schema.parser.FlusherService;
 import com.variant.core.schema.parser.SchemaParser;
 import com.variant.core.schema.parser.HooksService;
 
 /**
  * Client side schema parser is the same as core, but uses a null hooker
- * because there are no user hooks on the client.
+ * because there are no user hooks on the client, and a null flusher
+ * because we don't flush events on the client either.
  * 
  * @author Igor.
  *
@@ -13,8 +15,13 @@ import com.variant.core.schema.parser.HooksService;
 public class ClientSchemaParser extends SchemaParser {
 
 	@Override
-	protected HooksService getHooker() {
+	protected HooksService getHooksService() {
 		return HooksService.NULL;
+	}
+
+	@Override
+	protected FlusherService getFlusherService() {
+		return FlusherService.NULL;
 	}
 
 	
