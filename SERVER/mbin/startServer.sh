6a7a7a565896f209d7d8b100a936d19df584f50b
iurisman <igor.urisman@gmail.com>
2017-10-08 14:57:59 -0700

    Add variant.ext.dir

--- SERVER/mbin/startServer.sh
+++ SERVER/mbin/startServer.sh
@@ -1,9 +1,11 @@
 #! /bin/bash
 
 cd `dirname $0`/../../SERVER
+rootdir=$(pwd)
 
 rm -rf target/universal/stage/RUNNING_PID
 lsof -n -i4TCP:5377 | grep LISTEN | awk '{print $2}' | xargs kill
 
-config=${1:-'conf-test/variant-testProd.conf'}
-sbt "testProd -Dvariant.config.file=$config -Dhttp.port=5377"
+config=${1:-${rootdir}/conf-test/variant-testProd.conf}
+extdir=${rootdir}/distr/ext
+sbt "testProd -Dvariant.config.file=$config -Dvariant.ext.dir=$extdir -Dhttp.port=5377"
