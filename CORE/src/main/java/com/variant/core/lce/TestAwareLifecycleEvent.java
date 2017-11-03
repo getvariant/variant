50879f60591a3796100c58dd41a372347c33c585
iurisman <igor.urisman@gmail.com>
2017-11-03 15:42:16 -0700

    Typo

--- CORE/src/main/java/com/variant/core/lce/TestAwareLifecycleEvent.java
+++ CORE/src/main/java/com/variant/core/lce/TestAwareLifecycleEvent.java
@@ -3,7 +3,7 @@ package com.variant.core.lce;
 import com.variant.core.schema.Test;
 
 /**
- * <p>p>Super-interface for all life cycle event types, whose scope is limited to a particular test.
+ * <p>Super-interface for all life cycle event types, whose scope is limited to a particular test.
  * 
  * @author Igor Urisman.
  * @since 0.7
