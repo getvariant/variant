7b9b533b956d2d82e866971cb46b3536ad295033
iurisman <igor.urisman@gmail.com>
2017-11-03 21:09:13 -0700

    Rename key

--- SERVER/app/com/variant/server/api/ConfigKeys.java
+++ SERVER/app/com/variant/server/api/ConfigKeys.java
@@ -10,7 +10,7 @@ public interface ConfigKeys {
 	public static final String SCHEMATA_DIR               = "variant.schemata.dir";	
 	public static final String MAX_CONCURRENT_CONNECTIONS =  "variant.max.concurrent.connections";
 	public static final String SESSION_TIMEOUT            = "variant.session.timeout";
-	public static final String SESSION_VACUUM_INTERVAL    =  "variant.session.store.vacuum.interval";
+	public static final String SESSION_VACUUM_INTERVAL    =  "variant.session.vacuum.interval";
 	public static final String EVENT_FLUSHER_CLASS_NAME   = "variant.event.flusher.class.name";
 	public static final String EVENT_FLUSHER_CLASS_INIT   = "variant.event.flusher.class.init";
 	public static final String EVENT_WRITER_BUFFER_SIZE   =  "variant.event.writer.buffer.size";
